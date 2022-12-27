94fe365d072475aa90d76bdc12123a0ef5e280fd
inductionotg <ritesh2000.sinha@gmail.com>
2022-12-27 18:35:10 +0530

    Added unique constraint in database and completed the city repository

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express=require("express");
+
 const bodyParser = require('body-parser')
-const {PORT} = require('./config/serverConfig')
-console.log(PORT)
+
+const {PORT} = require('./config/serverConfig');
+const {City} = require("./models/index");
+const CityRepository = require('./repository/city-repository')
+
+
 const setupAndStartServer=async ()=>{
     const app = express();
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended:true}))
 
-    app.listen(PORT,()=>{
+    app.listen(PORT, ()=>{
         console.log(`server started at ${PORT}`)
+        //const repo = new CityRepository
+        //repo.createCity({name:'mumbai'})
+        //repo.deleteCity(2)
+       
     })
 
 }
